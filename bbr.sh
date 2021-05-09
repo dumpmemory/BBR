425e23721d90aa99c3525ae47e0a394b41c119eb
CoiaPrant <42506847+CoiaPrant@users.noreply.github.com>
2021-05-10 01:22:22 +0800

    v1.0.0

--- bbr.sh
+++ bbr.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
@@ -7,7 +7,7 @@ export PATH
 #	Description: BBR BBR魔改版 BBRPlus
 #	Author: CoiaPrant
 #=================================================
-a
+
 sh_ver="1.0.0"
 github="https://git.zeroteam.top/https://github.com/CoiaPrant/BBR/blob/master/"
 
